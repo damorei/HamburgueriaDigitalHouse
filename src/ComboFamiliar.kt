a5bb7d2673cf8339e7fff4868c7800c10b0dc3d8
Bruno Saunders <senabruno97@hotmail.com>
2020-09-21 21:23:23 -0300

    desconto promocional como atributo

--- src/ComboFamiliar.kt
+++ src/ComboFamiliar.kt
@@ -1,9 +1,11 @@
 class ComboFamiliar (vararg combo: Combo) : ItemDeCarrinho() {
     override var preco :Double = 0.00
     var listaProdutos = mutableListOf<Combo>()
+    val descontoPromocional: Double = 0.1
+
     init{
         listaProdutos.addAll(combo)
         listaProdutos.forEach{preco += it.preco}
-        preco *= 0.9
+        preco *= (1 - descontoPromocional)
     }
 }
